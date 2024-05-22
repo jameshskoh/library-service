b31910ea19d7e71f124de34484a793089c31ccc4
James Koh <huisoonkoh@gmail.com>
2024-05-22 14:21:12 +0800

    Create BookController

---
+++ src/main/java/com/jameshskoh/library/controller/BookController.java
@@ -0,0 +1,24 @@
+package com.jameshskoh.library.controller;
+
+import com.jameshskoh.library.model.Book;
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("books")
+public class BookController {
+
+  @GetMapping("list")
+  public List<Book> getAllBooks() {
+    // #TODO implement this
+    return List.of();
+  }
+
+  @PostMapping("create")
+  public void createBook() {
+    // #TODO implement this
+  }
+}
